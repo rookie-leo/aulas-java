dfdc33fde281fece234e780774010c7ab6169310
rookie-leo <proleonardopa@gmail.com>
2020-05-21 17:12:17 -0300

    Calculadora desenvolvida durante aula Curso em Video

--- PraticandoJava/src/praticandojava/PraticandoJava.java
+++ PraticandoJava/src/praticandojava/PraticandoJava.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praticandojava;
 
-import java.util.Scanner;
+import java.util.Scanner;/*importa a bibliotaca Scanner para fazer leitura do 
+teclado*/
 
-/**
- *
- * @author user
- */
 public class PraticandoJava {
 
     /**
@@ -18,16 +10,19 @@ public class PraticandoJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner tcl = new Scanner(System.in);
+        Scanner tcl = new Scanner(System.in);//Torna a variavel tcl um objeto
         System.out.print("Digite o seu nome: ");
-        String nome = tcl.nextLine();
-        System.out.print(nome + " agora digite um número: ");
-        int num1 = tcl.nextInt();
+        String nome = tcl.nextLine();//entrada de dados para a variável nome
+        System.out.print(nome + " agora digite um número: ");//retorna a str digitada, e faz uma requisição de entrada type int
+        int num1 = tcl.nextInt();//entrada de dados type int
         System.out.print("Digite outro número: ");
-        int num2 = tcl.nextInt();
-        int soma = num1 + num2;
-        System.out.println("A soma entre os dois números digitados pelo" + nome+
-                " é " + soma);
+        int num2 = tcl.nextInt();//entrada de dados type int
+        int soma = num1 + num2;//faz a soma entre os dois valores int digitados pelo user
+        System.out.println("A soma entre os dois números digitados pelo " + nome
+                + " é " + soma);//retorna a frase com o nome digitado concatenado com o valor da soma realizada
+        System.out.print("Digite um número: ");
+        float x1 = tcl.nextFloat();
+        System.out.println(x1);
     }
     
 }
